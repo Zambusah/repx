2d1b0e02b221449770adaa83c524ff7234f24777
gab <adfkdnf@email.com>
2025-06-20 18:21:21 +0200

    home

--- users/urls.py
+++ users/urls.py
@@ -10,8 +10,8 @@ path('schedules/', include('schedules.urls')),
 """
 
 urlpatterns=[
+path('', views.home_view, name='home'),
 path('admin/', admin.site.urls),
-path('', views.home_view, name='home'),  # homepage
 path('register/', views.register_view, name='register'),
 path('login/', views.login_view, name='login'),
 path('logout/', views.logout_view, name='logout'),

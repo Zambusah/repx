41d45f31f3277d1e37faa914f62cb89fa0346fc0
gab <adfkdnf@email.com>
2025-06-20 18:51:14 +0200

    finale

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from users.views import home_view
 from django.contrib.auth import views as auth_views
 
 """

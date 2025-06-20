737f2ed79c60f0fd430dbb273db6c6da5242b4ff
gab <adfkdnf@email.com>
2025-06-20 18:14:13 +0200

    dannata AI

--- users/urls.py
+++ users/urls.py
@@ -3,13 +3,15 @@ from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
+"""
+path('users/', include('users.urls')),
+path('bookings/', include('bookings.urls')),
+path('schedules/', include('schedules.urls')),
+"""
 
 urlpatterns=[
 path('admin/', admin.site.urls),
 path('', views.home_view, name='home'),  # homepage
-path('users/', include('users.urls')),
-path('bookings/', include('bookings.urls')),
-path('schedules/', include('schedules.urls')),
 path('register/', views.register_view, name='register'),
 path('login/', views.login_view, name='login'),
 path('logout/', views.logout_view, name='logout'),

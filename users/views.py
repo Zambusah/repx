5c06645354331391998177ad8077b8249ecda92d
gab <adfkdnf@email.com>
2025-06-21 15:36:29 +0200

    dashboard_teacher

--- users/views.py
+++ users/views.py
@@ -264,9 +264,10 @@ def video_call_view(request, token):
 
 
 
-stripe.api_key = settings.STRIPE_SECRET_KEY
+
 @csrf_exempt
 def create_checkout_session(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
     session = stripe.checkout.Session.create(
         payment_method_types=['card'],
         line_items=[{
